4b361d0c14fda57aa40e63576e6810e98ed4c172
Kenichi Kato <k3nichikato@gmail.com>
2013-02-17 13:32:10 +0900

    initial import.
    try managing initial files.

---
+++ mkslink.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cd $(dirname $0)
+test -d backup || mkdir backup
+for f in `ls -a|egrep -v '.git|backup|readme|mkslink.sh|\.$'`
+do
+    test -f ${HOME}/$f && mv ${HOME}/$f backup/$f.`date +%Y%m%d%H%M` 
+
+    ln -s `pwd`/$f ${HOME}/$f
+done
+
