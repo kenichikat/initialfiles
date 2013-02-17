483b2cd40653fbe4d661db1ef0ae0a1bb9afb65f
Kenichi Kato <k3nichikato@gmail.com>
2013-02-17 13:56:59 +0900

    fix checking before replacing files.
    add checking symbolic link existence before replace initial files.

--- mkslink.sh
+++ mkslink.sh
@@ -5,6 +5,7 @@ test -d backup || mkdir backup
 for f in `ls -a|egrep -v '.git|backup|readme|mkslink.sh|\.$'`
 do
     test -f ${HOME}/$f && mv ${HOME}/$f backup/$f.`date +%Y%m%d%H%M` 
+    test -L ${HOME}/$f && mv ${HOME}/$f backup/$f.`date +%Y%m%d%H%M` 
 
     ln -s `pwd`/$f ${HOME}/$f
 done
