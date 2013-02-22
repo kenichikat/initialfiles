b48300595107e55c16182b51e2f53fdd7513178b
kenichikat <kenichikat@sl6-64-dev01.kat>
2013-02-23 08:05:11 +0900

    change readme file name.
    script ignore readme file when setup each environment.

--- mkslink.sh
+++ mkslink.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname $0)
 test -d backup || mkdir backup
-for f in `ls -a|egrep -v '.git|backup|readme|mkslink.sh|\.$'`
+for f in `ls -a|egrep -v '.git|backup|README.md|mkslink.sh|\.$'`
 do
     test -f ${HOME}/$f && mv ${HOME}/$f backup/$f.`date +%Y%m%d%H%M` 
     test -L ${HOME}/$f && mv ${HOME}/$f backup/$f.`date +%Y%m%d%H%M` 
