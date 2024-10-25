7cf77d95d629c4f3628591ae378028721b0a04f7
Kenichi KATO <k3nichikato@gmail.com>
2024-10-25 15:14:34 +0900

    Update install-docker-ubuntu.sh
    オプションをミスった

--- install-docker-ubuntu.sh
+++ install-docker-ubuntu.sh
@@ -20,4 +20,4 @@ sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo usermod -sG docker ${user}
+sudo usermod -aG docker ${user}
