58b2248eaa0a69f9064ff89cdb97ffbdbe6d4e40
Kenichi KATO <k3nichikato@gmail.com>
2025-01-13 17:55:18 +0900

    frame設定をととめた

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,7 +3,10 @@ local wezterm = require 'wezterm'
 config = wezterm.config_builder()
 
 config.automatically_reload_config = true
-config.font_size = 10.0
+config.window_frame = {
+    font = require('wezterm').font 'Roboto',
+    font_size = 10.0
+}
 config.window_background_opacity = 0.80
 
 config.use_ime = true
