c0cac4b0d75ebeededebb14d891d6e14b659d79f
Kenichi KATO <k3nichikato@gmail.com>
2025-02-16 22:15:52 +0900

    modifty startup size

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require 'wezterm'
-
-config = wezterm.config_builder()
+local config = wezterm.config_builder()
+local mux = wezterm.mux
 
 config.automatically_reload_config = true
 config.window_frame = {
@@ -10,5 +10,11 @@ config.window_frame = {
 config.window_background_opacity = 0.80
 
 config.use_ime = true
+
+wezterm.on('gui-startup', function(cmd)
+    local tab, pane, window = mux.spawn_window(cmd or {width=240, height=60})
+    window:gui_window():set_position(0,0)
+    window:set_inner_size(1600, 900)
+  end)
 
 return config
